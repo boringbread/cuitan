aa3c66cda9a60dda88378c6eadea71679544e5a0
Yohan Kurnia Wijaya <yohanhuang300999@gmail.com>
2021-01-07 11:42:30 +0700

    [ADD] Comments

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -9,31 +9,48 @@ use App\Tweets;
 
 class ProfileController extends Controller
 {
+
+    /*
+        Melihat Profile berdasarkan parameter username
+        url:    /profile/{username}
+        contoh penggunaan dynamic route: href="{{route('profile.view',$username)}}"
+    */
     public function viewProfile($username){
+        //cari user berdasarkan username
         $user = User::where('username',$username)->first();
 
         if($user->followers)$user->follower_count = count($user->followers);
         if($user->following)$user->following_count = count($user->following);
 
+        //jika user tidak ditemukan, arahkan kembali ke halaman index
         if(!$user){return redirect(route('index'));}
 
+        //ambil tweet milik user tersebut
         $tweets = Tweets::where('id_user',$user->id)->get();
 
+        //lengkapi data yang diperlukan tweet
         foreach($tweets as $tweet){
             $tweet->disp_name = $user->disp_name;
             $tweet->username = $user->username;
         }
 
+        //role pada lihat profile
         $role = "default";
 
+        //jika sudah login
         if(Auth::check()){
+            //jika current user (login) merupakan pemilik user yang dilihat, role adalah owner
             if(Auth::user()->id == $user->id){
                 $role = "owner";
-            }else{
+            }
+            //jika bukan, maka hanya 'visit'or
+            else{
                 $role = "visit";
             }
         }
 
+        //bungkus data seperti gilang membungkus korbannya
+        //sangat rapih
         $data = [
             'user'=>$user,
             'tweets'=>$tweets,
@@ -43,21 +60,61 @@ class ProfileController extends Controller
         return view('profile.view')->with($data);
     }
     
-    //kasi unfollow
+    /*
+        Follow user tertentu berdasarkan id
+        url:   /profile/{id}
+        contoh penggunaan dynamic route: href="{{route('profile.follow',$id)}}"
+    */
     public function followProfile($id){
+        #Todo: - tambahkan ::middleware pada route
         $user = Auth::user();
 
+        //cari profile user yang ingin difollow
         $toFollow = User::where('_id',$id)->first();
 
-        //punya si follower
+        //batalkan follow jika tidak ditemukan profile user
+        if(!$toFollow){return redirect()->back();}
+
+        //tambahkan id user yang ingin difollow pada array following diri sendiri (login)
         $user->push('following', $id, true);
         $user->save();
         
-        //punya to Follow
+        //tambahkan id user diri sendiri pada array follower user yang ingin difollow
         $toFollow->push('followers',$user->id, true);
         $toFollow->save();
 
+        //kembalikan user ke halaman profil
         return redirect( route('profile.view', $toFollow->username) );
+    }
+
+    /*
+        Unfollow user tertentu berdasarkan id
+        url:   /profile/unfollow/{id}
+        contoh penggunaan dynamic route: href="{{route('profile.unfollow',$id)}}"
+    */
+    public function unfollowProfile($id){
+        /* 
+            INI GW MASI ASAL COPAS DOANG WKWKWK
+            BELOM ADA DI ROUTE, VIEW
+         */
+
+        // #todo: ::middleware pada route
+        
+        // $user = Auth::user();
+
+        // //cari profile user yang ingin diunfollow
+        // $toUnfollow = User::where('_id',$id)->first();
+
+        // //batalkan unfollow jika tidak ditemukan profile user
+        // if(!$toUnfollow){return redirect()->back();}
+
+        // //tambahkan id user yang ingin diunfollow pada array following diri sendiri (login)
+        // $user->pull('following', $id, true);
+        // $user->save();
+        
+        // //tambahkan id user diri sendiri pada array follower user yang ingin difollow
+        // $toUnfollow->pull('followers',$user->id, true);
+        // $toUnfollow->save();
     }
 
     public function deleteAccount(){
--- app/Http/Controllers/TweetController.php
+++ app/Http/Controllers/TweetController.php
@@ -9,16 +9,32 @@ use App\User;
 
 class TweetController extends Controller
 {
+    /*
+        Melihat Tweet berdasarkan parameter ID
+        url:    /status/{tweet_id}
+        contoh penggunaan dynamic route: href="{{route('tweet.view',$id)}}"
+    */
     public function viewTweet($tweet_id){
+        //mencari tweet pada database(where), dan mengambil hasil pencariannya(first)
         $tweet = Tweets::where('_id',$tweet_id)->first();
+
+        //jika tweet tidak ditemukan, arahkan user ke halaman index
         if(!$tweet){
             return redirect(route('index'));
         }
-        //todo: ambil kalo ada, parentnya, replynya
+
+        //Ambil Tweet Ancestor dan Predecessor jika ada, dan lakukan append pada objek Tweet yang ditemukan tadi
         if($tweet->reply_anc){
+            //cari tweet ancestor melalui IDnya.
             $ancTweet = Tweets::where('_id',$tweet->reply_anc)->first();
+
+            //Jika ditemukan, tambahkan field-field yang diperlukan oleh tweet seperti display name dan username.
             if($ancTweet){
+
+                //dapatkan objek user pemilik tweet untuk mendapatkan informasi disp_name dan username
                 $user_anc = User::where('_id',$ancTweet->id_user)->first();
+
+                //append data objek user pemilik pada objek tweet
                 $ancTweet->disp_name = $user_anc->disp_name;
                 $ancTweet->username = $user_anc->username;
             }
@@ -28,79 +44,101 @@ class TweetController extends Controller
             //buat array baru
             $predeArray = array();
             
-            //foreach elemen pada reply prede
+            //foreach elemen pada reply prede, tahap ini mirip dengan reply_anc di atas
             foreach($tweet->reply_prede as $prede){
                 $predeTweets = Tweets::where('_id',$prede)->first();
                 if($predeTweets){
                     $user_prede = User::where('_id',$tweet->id_user)->first();
                     $predeTweets->disp_name = $user_prede->disp_name;
                     $predeTweets->username = $user_prede->username;
+                    //masukan objek ke array baru
                     array_push($predeArray, $predeTweets);
                 }
             }
-
+            //append array pada objek tweet
             $tweet->prede = $predeArray;
             $tweet->count_prede = count($tweet->prede);
         }
         
-        /*getting user data*/
+        //Lengkapi data yang dibutuhkan tweet
         $user = User::where('_id',$tweet->id_user)->first();
         $tweet->disp_name = $user->disp_name;
         $tweet->username = $user->username;
 
+        //kembalikan tampilan pages.tweet dan render tampilan dengan data $tweet
         return view('pages.tweets')->with('tweet',$tweet);
     }
 
+    /*
+        Mem-post Tweet baru (bukan reply / retweet)
+        url:    /tweet/post
+    */
     public function postTweet(Request $request){
-        //todo: validate AUTH & validate tweet FIELDS
+        #todo: - tambahkan ::middleware pada route delete
+        #      - validasi isi field tweet (length tweet)
         $user = Auth::user();
         $tweet = new Tweets();
 
         $tweet->text = $request->input('text');
         $tweet->id_user = $user->id;
         $tweet->date_added = date("d/m/Y h:i:s");
+
+        //simpan tweet ke database
         $tweet->save();
 
+        //arahkan user ke halaman index
         return redirect(route('index'));
     }
 
+    /*
+        Membuat Tweet Reply
+    */
     public function replyTweet(Request $request){
-        //add middleware
+        #todo: - tambahkan ::middleware pada route reply
+        #      - validasi isi field tweet (length tweet)
+
+        //ambil parameter ID Tweet yang direply dari form
         $id = $request->input('reply_id');
-        $tweet = Tweets::where('_id', $id)->first();
+        //cari tweet berdasarkan parameter ID Tweet
+        $tweet = Tweets::where('_id',$id)->first();
+        //lakukan cek jika tweet ada, dan arahkan jika tidak ada
+        /*belum diimplementasikan*/
+
         $newTweet = new Tweets();
         $user = Auth::user();
 
-        // dd($tweet);
+        //lengkapi data tweet baru
         $newTweet->text = $request->input('reply_data');
         $newTweet->id_user = $user->id;
         $newTweet->date_added = date("d/m/Y h:i:s");
         $newTweet->reply_anc = $tweet->id;
+        //simpan data tweet baru pada database
         $newTweet->save();
 
+        //tambahkan ID Tweet baru dalam array predecessor tweet yang direply
         $tweet->push('reply_prede',$newTweet->id, true);
         $tweet->save();
 
+        //arahkan kembali user pada halaman tweet yang direply
         return redirect(route('tweet.view',$tweet->id));
     }
 
+    /*
+        Menghapus Tweet
+    */
     public function deleteTweet(Request $request, $id){
+        #todo: tambahkan ::middleware pada route delete
+
+        //cari tweet berdasarkan ID
         $tweet = Tweets::where('_id', $id)->first();
 
-        $ancTweet = Tweets::where('_id',$tweet->reply_anc)->first();
-        $predeTweets = array();
+        //cek apakah tweet benar milik user yang login saat ini (Auth::user())
+        /*belum diimplementasikan*/
 
-        foreach($tweet->reply_prede as $id){
-            $prede = Tweets::where('_id',$id)->first();
-            if($prede){
-                $prede->delete();
-            }
-        }
-        if($ancTweet){
-
-        }
+        //hapus tweet dari database
         $tweet->delete();
-
+        
+        //arahkan user ke halaman index
         return redirect(route('index'));
     }
     
