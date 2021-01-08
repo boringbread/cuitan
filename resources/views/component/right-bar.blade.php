0cc59a7c4a7d48d765272d56c2447b08cf35154d
Muhammad Prasasta <prasastaa@gmail.com>
2021-01-09 00:57:59 +0700

    [UPDATE] Who to follow appear when logged in

--- resources/views/component/right-bar.blade.php
+++ resources/views/component/right-bar.blade.php
@@ -1,27 +1,29 @@
 <div class="container-fluid border-left rbar" style="position: sticky; top: 0rem; height: 100vh;overflow-y:scroll">
     @include('component.developer')
 
+    @if(Auth::check())
     <div class="pb-2">
         <div class="pt-3 mt-3 pb-2 border-bottom border-top">
             <h5 class="">Who to Follow</h5>   
         </div>
 
-        @if (Cache::get('whotofollow') == null)
-            <?php App\Http\Controllers\C_Index::getWTF();?>
+            @if (Cache::get('whotofollow') == null)
+                <?php App\Http\Controllers\C_Index::getWTF();?>
+            @endif
+            
+            @foreach (Cache::get('whotofollow') as $item)
+                <div class="mt-4 d-flex border rounded px-2 py-3 svg">
+                    <a href="{{ route('profile.view', $item->username) }}" class="d-flex">
+                        <div class="ml-1">
+                            <img src="{{asset('img/profile/'.($item->pphoto!=NULL? $item->pphoto:"noimg.png"))}}" class="" width="50px" alt="" style=" clip-path: circle();">
+                        </div>
+                        <div class="ml-3">
+                            <h6>{{ $item->disp_name }}</h6>
+                            <h6 class="text-muted">{{ $item->username }}</h6>
+                        </div>
+                    </a>
+                </div>
+            @endforeach
+        </div>
         @endif
-        
-        @foreach (Cache::get('whotofollow') as $item)
-            <div class="mt-4 d-flex border rounded px-2 py-3 svg">
-                <a href="{{ route('profile.view', $item->username) }}" class="d-flex">
-                    <div class="ml-1">
-                        <img src="{{asset('img/profile/'.($item->pphoto!=NULL? $item->pphoto:"noimg.png"))}}" class="" width="50px" alt="" style=" clip-path: circle();">
-                    </div>
-                    <div class="ml-3">
-                        <h6>{{ $item->disp_name }}</h6>
-                        <h6 class="text-muted">{{ $item->username }}</h6>
-                    </div>
-                </a>
-            </div>
-        @endforeach
-    </div>
 </div>
